ed4c5c65a11751f01c406a7f9b076958925559ec
StilianKaridov <karidovstilian@gmail.com>
2023-03-27 17:10:41 +0300

    Initial design for the Navigation Bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function NavBar() {
+function NavBar(props) {
 
-  return (
-    <Button variant="primary">Click me</Button>
-  );
+    return (
+        <Router>
+            <Navbar bg="light" expand="lg">
+                <LinkContainer to="/">
+                    <Navbar.Brand>My App</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <LinkContainer to="/home">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/about">
+                            <Nav.Link>About</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/contact">
+                            <Nav.Link>Contact</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Router>
+
+    );
 }
 
 export default NavBar;
