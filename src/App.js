ef72fc64ae705088f2300e817ea757ac2fcba361
StilianKaridov <karidovstilian@gmail.com>
2023-03-30 21:13:51 +0300

    Added Routes and Link

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import BackToTopButton from './components/BackToTopButton/BackToTopButton';
+import { Routes, Route } from 'react-router-dom';
+import Hello from './components/Muje';
+import { Link } from 'react-router-dom';
+
 
 function App() {
-  const button = "natisni";
-
   return (
     <div>
-      <NavBar text={button}>
-
+      <NavBar>
       </NavBar>
 
       <Footer>
       </Footer>
-      
-      <BackToTopButton>
 
-      </BackToTopButton>
+      //Да се пробвам да ги сложа в футъра
+        <Routes>
+          <Route path='/products/muje' element={<Hello />}/>
+        </Routes>
+      <Link to={"/products/muje"}>Мъжки</Link>
+
+
+      <BackToTopButton />
     </div>
 
   );
