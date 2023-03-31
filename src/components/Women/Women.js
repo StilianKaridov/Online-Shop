b21df2179dac411d72d89b7892813797df87ea18
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2023-03-31 17:28:05 +0300

    Added cards to display items

--- src/components/Women/Women.js
+++ src/components/Women/Women.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { women } from '../../data';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
+//Трябва да се подобри
 export default function Women() {
 
     return (
         //Щe трбва да измислим нещо общо за тези li и така ще го преизползваме в другите компоненти
-        <ul>
-            {women.map(item =>
-                <div>
-                    <li>{item.url}</li>
-                    <li>{item.description}</li>
-                    <li>{item.sale}</li>
-                    <li>{item.price}</li>
-                </div>
-            )}
-        </ul>
+        <Container>
+            <Row xs={1} md={4}>
+                {women.map(item =>
+                    <Col>
+                        <Card>
+                            <Card.Img>{item.url}</Card.Img>
+                            <Card.Body>{item.description}</Card.Body>
+                            <Card.Text>Price: {item.price}</Card.Text>
+                        </Card>
+                    </Col>
+                )}
+            </Row>
+        </Container>
     );
 }
