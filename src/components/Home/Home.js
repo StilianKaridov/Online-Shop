1c243b1e42398e79ef0f1aab576f1786a0f8104c
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2023-03-31 17:09:42 +0300

    Added Children and Home javascript files

---
+++ src/components/Home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        //В тази страница да връщаме някви промоции например !! да се помисли.
+        <h1>Home component</h1>
+    );
+}
