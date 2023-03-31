132fff7a593db122d9d0ac16dfc3985a0310f472
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2023-03-31 17:09:00 +0300

    Muje.js changed to Men.js and moved to Men folder

---
+++ src/components/Men/Men.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { men } from '../../data';
+
+export default function Men() {
+    console.log(men);
+    return (
+        <h1>Hello from men component</h1>
+    );
+}
