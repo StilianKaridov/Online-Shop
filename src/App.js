1d8cda711408ca2b2c369cbd5d97ea6ace0bc273
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2023-03-31 17:10:46 +0300

    Updated App.js to work with links

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import BackToTopButton from './components/BackToTopButton/BackToTopButton';
-import { Routes, Route } from 'react-router-dom';
-import Hello from './components/Muje';
-import { Link } from 'react-router-dom';
+import Children from './components/Children/Children';
+import Women from './components/Women/Women';
+import Men from './components/Men/Men';
+import { Routes, Route, Link } from 'react-router-dom';
+
 
 
 function App() {
@@ -12,17 +14,20 @@ function App() {
     <div>
       <NavBar>
       </NavBar>
-
-      <Footer>
-      </Footer>
-
-      //Да се пробвам да ги сложа в футъра
-        <Routes>
-          <Route path='/products/muje' element={<Hello />}/>
-        </Routes>
-      <Link to={"/products/muje"}>Мъжки</Link>
-
-
+      {/* Ще трябва да го променим в процеса най-вероятно. Tова е да държи footer долу */}
+      <div className='flex-wrapper min-vh-100 d-flex flex-column justify-content-between'>
+        <main>
+          {/* //Да се пробвам да ги сложа в футъра */}
+          <Routes >
+            <Route path='/products/muje' element={<Men />} />
+            <Route path='/products/jeni' element={<Women />} />
+            <Route path='/products/deca' element={<Children />} />
+          </Routes>
+          <Link to={"/products/muje"}>Мъжки</Link>
+        </main>
+        <Footer>
+        </Footer>
+      </div>
       <BackToTopButton />
     </div>
 
