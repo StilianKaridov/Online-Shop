f741de111898783c99900497f2072fbccc300298
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2023-03-31 16:57:16 +0300

    Dummy data file for testing

---
+++ src/data.js
@@ -0,0 +1,85 @@
+//Да измислим какви променливи за тез дрехи
+export const men = [
+    {
+        //type : тук може да е какъв е самия атрикул / обувка или нещо там / 
+        img: "url",
+        description: "Obuvki",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "Pantalon",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "Riza",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "chorapi",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "neshto",
+        sale: true,//false
+        price: "12.50",
+    }
+];
+
+export const women = [
+    {
+        img: "url",
+        description: "Obuvki",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "Pantalon",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "drugo",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "chorapi",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "neshto",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "neshto",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "neshto",
+        sale: true,//false
+        price: "12.50",
+    },
+    {
+        img: "url",
+        description: "neshto",
+        sale: true,//false
+        price: "12.50",
+    }
+]
