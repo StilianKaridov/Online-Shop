f3550681bd11f31c49ba305156772e01b13e0116
StilianKaridov <karidovstilian@gmail.com>
2023-04-28 15:56:24 +0300

    Added the OverNavBar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import OverNavBar from './components/NavBar/OverNavBar';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Routes from './components/Routes/Routes';
@@ -8,8 +9,9 @@ import BackToTopButton from './components/BackToTopButton/BackToTopButton';
 function App() {
   return (
     <div>
-      <NavBar>
-      </NavBar>
+      <OverNavBar />
+      
+      <NavBar />
 
       {/* Ще трябва да го променим в процеса най-вероятно. Tова е да държи footer долу */}
       <div className='flex-wrapper min-vh-100 d-flex flex-column justify-content-between'>
