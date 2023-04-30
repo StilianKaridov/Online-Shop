0b0a03c1ca7d9ad46cef1e39ebecb89acb4d08ba
StilianKaridov <karidovstilian@gmail.com>
2023-04-30 19:34:19 +0300

    Added three buttons for Man, Woman, Kids

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,39 @@
 import React from "react";
 
-import MainImage from '../../images/MainImage.mp4'
+import MainImage from '../../images/MainImage.mp4';
+import css from "./file.css";
+import { Container, Button } from "react-bootstrap";
 
 export default function Home() {
     return (
-        <video playsInline autoPlay muted style={{ maxWidth: "100%" }}>
-            <source src={MainImage} type="video/mp4" />
-        </video>
+
+        <Container fluid className="p-0" style={{ position: "relative" }}>
+            <video style={{ maxWidth: "100%" }} playsInline autoPlay muted>
+                <source src={MainImage} type="video/mp4" />
+            </video>
+
+            <div style={{ position: 'absolute', top: '30%', left: '50%', transform: 'translate(-50%, -50%)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Button
+                    href="/products/muje"
+                    className="button mx-2"
+                    style={{ minWidth: '100px', backgroundColor: 'rgb(191, 128, 64)', border: 'none' }}>
+                    Мъжки
+                </Button>
+                
+                <Button
+                    href="/products/jeni"
+                    className="button mx-2"
+                    style={{ minWidth: '100px', backgroundColor: 'rgb(191, 128, 64)', border: 'none' }}>
+                    Дамски
+                </Button>
+
+                <Button
+                    href="/products/deca"
+                    className="button mx-2"
+                    style={{ minWidth: '100px', backgroundColor: 'rgb(191, 128, 64)', border: 'none' }}>
+                    Детски
+                </Button>
+            </div>
+        </Container>
     );
 }
